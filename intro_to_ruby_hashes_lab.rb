4cf462ddb63988777cd2d040b54a17bcc461e0e0
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-15 14:40:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,9 @@ hash = {:railroads =>
                 {
                   :reading_railroad => {},
                   :pennsylvania_railroad => {},
+                  :b_and_o_railroad => {},
                   :b_and_o_railroad => {}
+                  
                 }, 
               :rent_in_dollars => 
                 {
