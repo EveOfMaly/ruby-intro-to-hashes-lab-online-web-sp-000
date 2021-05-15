fe00cc8866f94cea2fc372b081d2c64058efe9c2
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-15 14:24:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  hash = {:railroads => {:pieces => 4}, :names => {}}
+  hash = {:railroads => {:pieces => 4}, :names => hash.new
 end
 
 def monopoly_with_third_tier
